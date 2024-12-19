f6a308a5429eca39d4eb23a8e5eb38371f2497e0
Ahmedalihh <ahmedalibbb227@gmail.com>
2024-12-19 23:12:26 +0500

    if else

---
+++ app.js
@@ -0,0 +1,100 @@
+// var gender = "male";
+// console.log(gender);
+// if statament
+// if (conditions) {
+// block of statement
+// } else{
+
+// }
+// var num1 = 10;
+
+// console.log(num1, "num1");
+
+// var gender = "female";
+// if (fender == "male") {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+//
+// => assigning opretoe
+// ==> equal too
+// ===> value check and data type
+
+// var a = "10";
+// if (a == 10) {
+//   console.log("OK!");
+// } else {
+//   console.log("error");
+// }
+
+// var num1 = 100;
+// if (typeof num1 == "number") {
+//   console.log("number type");
+// } else {
+//   console.log("type not match");
+// }
+
+// var age = +prompt("enter your age");
+// if (age === 18) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// var num1 = +prompt("enter number 1");
+// var num2 = +prompt("enter number 2");
+
+// var opt = prompt("enter opt", "+,-,*,/");
+// console.log(num1);
+// console.log(num2);
+
+// console.log(opt);
+// if (opt == "+") {
+//   console.log("addition", num1 + num2);
+// } else if (opt == "-") {
+//condition changing
+//   console.log("sub", num1 - num2);
+// } else if (opt == "*") {
+//   console.log("mul", num1 * num2);
+// } else if (opt == "/") {
+//   console.log("/", num1 / num2);
+// } else {
+//   console.log("Invalid opt");
+// }
+
+// 18,20,25 30 ==>allow
+
+// var age = 10;
+// if (age == 25) {
+//   console.log("allow");
+// } else if (age == 20) {
+//   console.log("allow");
+// } else if (age == 18) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// if (age == 20) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
+// var gender = "male";
+// if (gender == "male") {
+//   console.log("larka hai");
+// } else {
+//   console.log("tiktoker hai");
+// }
+
+// comparision operator
+// >
+// <
+// >=
+// <=
+
+// var age = prompt("enter your age");
+// if (age >= 18) {
+//   console.log("allow");
+// } else {
+//   console.log("not allow");
+// }
